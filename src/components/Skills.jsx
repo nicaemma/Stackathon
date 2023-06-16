a297ca404df6db0a771703e40e1267a8d2e22a9c
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-15 21:57:17 -0400

    Coping skills form

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-indigo-500`,
+};
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>My Coping Skills</h3>
+        <form className={style.form}>
+          <input
+            className={style.input}
+            type="text"
+            placeholder="Add coping skill"
+          />
+          <button className={style.button}>
+            <AiOutlinePlus />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
