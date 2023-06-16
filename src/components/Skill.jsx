30916d3f1754b41d8f74b5aee891ec04057f56f6
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-16 18:14:57 -0400

    Styled individual skills

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
+
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer flex items-center`,
+};
 
 const Skill = ({ skill }) => {
-  return <li>{skill}</li>;
+  return (
+    <li className={style.li}>
+      <div className={style.row}>
+        <input type="checkbox" />
+        <p className={style.text}>{skill}</p>
+      </div>
+      <button>{<FaRegTrashAlt />} </button>
+    </li>
+  );
 };
 
 export default Skill;
