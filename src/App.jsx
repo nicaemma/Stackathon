5fa7e64e6e792adf16389152c4d1658c1f44d517
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-15 21:09:39 -0400

    install react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div>App.js</div>
+    </>
+  );
 }
 
 export default App;
