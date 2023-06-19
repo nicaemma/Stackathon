aa82b00bacbda05372945d835cc400ccd1d37a8d
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-19 17:34:03 -0400

    Reset password works

--- src/components/User-Auth/ForgotPassword.jsx
+++ src/components/User-Auth/ForgotPassword.jsx
@@ -3,18 +3,44 @@ import { UserAuth } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
 
 const ForgotPassword = () => {
-  const emailRef = useRef();
+  const [email, setEmail] = useState("");
   const { resetPassword } = UserAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      // Passing in whatever you put in email box:
+      await resetPassword(email);
+      await setEmail("");
+      setMessage("Check your inbox for further instructions");
+    } catch {
+      setError("Failed to reset password");
+      setLoading(false);
+    }
+  }
 
   return (
     <div className="'max-w-[700px] mx-auto my-16 p-4">
       <div>
         <h1 className="text-2xl font-bold py-2">Reset Password</h1>
       </div>
-      <form>
+      {error && <div>{error}</div>}
+      {message && <div>{message}</div>}
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col py-2">
           <label className="py-2 font-medium">Email Address</label>
-          <input ref={emailRef} className="border p-3" type="email" />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="border p-3"
+            type="email"
+          />
         </div>
 
         <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
