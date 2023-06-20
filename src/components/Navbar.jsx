87c41e1825c4f3d03b97d11f90dfd9eef8f96653
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-20 16:33:46 -0400

    Navbar initial styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className=" bg-[#c7d2fe]">
+      <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+        <h1 className="w-full text-3xl font-bold text-[#3730a3]">
+          Mindful Matters
+        </h1>
+        <ul className="flex">
+          <li className="p-8">Home</li>
+          <li className="p-8">My Self Care</li>
+          <li className="p-8">Activities</li>
+          <li className="p-8">Resources</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
