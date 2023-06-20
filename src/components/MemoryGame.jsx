c05941c8464b53c605cbc94ed0f09c5bcd182303
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-20 16:15:54 -0400

    Memory component inital styling

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const MemoryGame = () => {
-  return <div>MemoryGame</div>;
+  return (
+    <div className="p-20 bg-[#831843] w-full h-screen m-auto flex flex-col items-center">
+      <h1 className="text-white text-2xl text-center">Memory Game</h1>
+      <div className="max-w-[40%] w-fit h-fit m-5">
+        <button className="justify-center border border-white rounded-md p-2 text-white bg-[#3730a3] hover:bg-[#6366f1] cursor-pointer">
+          New Game
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MemoryGame;
