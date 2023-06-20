6a5db2a29ac3b67d0a817274a0d2d57a7fe1e463
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-20 10:23:20 -0400

    Edited structure of EditProfile form

--- src/components/User-Auth/EditProfile.jsx
+++ src/components/User-Auth/EditProfile.jsx
@@ -13,19 +13,39 @@ const EditProfile = () => {
     e.preventDefault();
   };
   return (
-    <div>
+    <div className="'max-w-[700px] mx-auto my-16 p-4">
+      <div>
+        <h1 className="text-2xl font-bold py-2">Edit Your Information</h1>
+
+        {/* {error && <div>{error}</div>} */}
+      </div>
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col py-2">
           <label className="py-2 font-medium">Name</label>
-          <input ref={nameRef} className="border p-3" type="name" />
+          <input
+            ref={nameRef}
+            className="border p-3"
+            type="name"
+            defaultValue={currentUser.displayName}
+          />
         </div>
         <div className="flex flex-col py-2">
           <label className="py-2 font-medium">Email Address</label>
-          <input ref={emailRef} className="border p-3" type="email" />
+          <input
+            ref={emailRef}
+            className="border p-3"
+            type="email"
+            defaultValue={currentUser.email}
+          />
         </div>
         <div className="flex flex-col py-2">
           <label className="py-2 font-medium">Password</label>
-          <input ref={passwordRef} className="border p-3" type="password" />
+          <input
+            ref={passwordRef}
+            className="border p-3"
+            type="password"
+            placeholder="Leave blank to keep the same"
+          />
         </div>
         <div className="flex flex-col py-2">
           <label className="py-2 font-medium">Confirm Password</label>
@@ -33,10 +53,11 @@ const EditProfile = () => {
             ref={passwordConfirmRef}
             className="border p-3"
             type="password"
+            placeholder="Leave blank to keep the same"
           />
         </div>
         <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-          Confirm Edits
+          Submit Edits
         </button>
       </form>
     </div>
