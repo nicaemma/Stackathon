0db9254f6f05742a9df199c8f373c9516487e6bf
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-20 17:23:10 -0400

    Font imports, styling changes to navbar and memory outline

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -2,12 +2,20 @@ import React from "react";
 
 const MemoryGame = () => {
   return (
-    <div className="p-20 bg-[#831843] w-full h-screen m-auto flex flex-col items-center">
-      <h1 className="text-white text-2xl text-center">Memory Game</h1>
-      <div className="max-w-[40%] w-fit h-fit m-5">
-        <button className="justify-center border border-white rounded-md p-2 text-white bg-[#3730a3] hover:bg-[#6366f1] cursor-pointer">
-          New Game
-        </button>
+    <div>
+      <img
+        className="w-full h-screen object-cover object-left"
+        src="https://img.freepik.com/premium-vector/dark-green-abstract-watercolor-texture-background_65186-958.jpg?w=2000"
+      />
+      <div className="absolute w-full h-screen top-10 left-0 font-fira">
+        <div className="p-20 m-auto flex flex-col items-center">
+          <h1 className="text-white text-2xl text-center">Memory Game</h1>
+          <div className="max-w-[40%] w-fit h-fit m-5">
+            <button className="justify-center border border-white rounded-md p-2 text-white bg-[#3730a3] hover:bg-[#6366f1] cursor-pointer">
+              New Game
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className=" bg-[#c7d2fe]">
+    <div className=" bg-[#c7d2fe] font-sora">
       <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
         <h1 className="w-full text-3xl font-bold text-[#3730a3]">
           Mindful Matters
         </h1>
-        <ul className="flex">
+        <ul className="flex flex-shrink">
           <li className="p-8">Home</li>
-          <li className="p-8">My Self Care</li>
+          <li className="p-8 whitespace-nowrap">My Self Care</li>
           <li className="p-8">Activities</li>
           <li className="p-8">Resources</li>
         </ul>
