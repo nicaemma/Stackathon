77be4006195d3c0593eff28a56797f04794c441f
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-21 15:41:49 -0400

    Card shuffle works in Memory Game

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+const cardImages = [
+  { src: "/img/memory/meditation-sillhouette.png" },
+  { src: "/img/memory/moon-reflection.png" },
+  { src: "/img/memory/pink-flowers.png" },
+  { src: "/img/memory/zen-stones-in-water.png" },
+  { src: "/img/memory/winter-city.png" },
+  { src: "/img/memory/tree-sky.png" },
+];
 
 const MemoryGame = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards --> runs at the start to initialize game.
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    // Map above: ...card --> each card's properties (src), id --> add this property to each card
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log("cards-->", cards);
+  console.log("turns-->", turns);
+
   return (
     <div>
       <img
@@ -11,7 +36,10 @@ const MemoryGame = () => {
         <div className="p-20 m-auto flex flex-col items-center">
           <h1 className="text-white text-2xl text-center">Memory Game</h1>
           <div className="max-w-[40%] w-fit h-fit m-5">
-            <button className="justify-center border border-white rounded-md p-2 text-white bg-[#3730a3] hover:bg-[#6366f1] cursor-pointer">
+            <button
+              onClick={shuffleCards}
+              className="justify-center border border-white rounded-md p-2 text-white bg-[#3730a3] hover:bg-[#6366f1] cursor-pointer"
+            >
               New Game
             </button>
           </div>
