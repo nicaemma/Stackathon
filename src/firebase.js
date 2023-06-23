e83a01a2f68f9396c5e950979ca3fee0595c640c
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-23 13:29:48 -0400

    transfer firebase variables to env

--- src/firebase.js
+++ src/firebase.js
@@ -7,6 +7,7 @@ PUT IN ENV FILE BEFORE DEPLOYING
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
+import { config } from "dotenv";
 
 //Auth
 import { getAuth } from "firebase/auth";
@@ -14,16 +15,17 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDqg-G4enp70cM_FOthKfEn0rZZtgt9cEU",
-  authDomain: "stackathon-68512.firebaseapp.com",
-  projectId: "stackathon-68512",
-  storageBucket: "stackathon-68512.appspot.com",
-  messagingSenderId: "331434438985",
-  appId: "1:331434438985:web:8a1a0a249d795972e32a98",
-  measurementId: "G-N0RKKB5GC8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+console.log("Firebase API key-->", firebaseConfig.apiKey);
 // import { getAnalytics } from "firebase/analytics";
 // const analytics = getAnalytics(app);
 
