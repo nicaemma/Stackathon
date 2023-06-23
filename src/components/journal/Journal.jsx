18ab05509916de1a912742512dc851fbae397b3a
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-23 16:15:24 -0400

    Spotify authorization works in Journal component

--- src/components/journal/Journal.jsx
+++ src/components/journal/Journal.jsx
@@ -4,7 +4,17 @@ const Journal = () => {
   const clientId = import.meta.env.VITE_CLIENT_ID;
   const redirectURI = import.meta.env.VITE_REDIRECT_URI;
   const authEndpoint = import.meta.env.VITE_AUTH_ENDPOINT;
-  return <div>Journal</div>;
+  const responseType = import.meta.env.VITE_RESPONSE_TYPE;
+  return (
+    <div>
+      <h1>Journal</h1>
+      <a
+        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectURI}&response_type=${responseType}`}
+      >
+        Login to Spotify
+      </a>
+    </div>
+  );
 };
 
 export default Journal;
