b1686be9de25e19a4fc1cc5798a93b6e591dfa38
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-24 13:41:10 -0400

    Setup background image and container in journal

--- src/components/journal/Entry.jsx
+++ src/components/journal/Entry.jsx
@@ -3,9 +3,9 @@ import { MdDeleteForever } from "react-icons/md";
 
 const Entry = () => {
   return (
-    <div className="bg-[#f3e8ff] flex flex-col justify-between p-2 mb-10 border-r-10 min-h-[170px]">
+    <div className="bg-[#eef2ff] flex flex-col justify-between p-2 mb-10 rounded-lg drop-shadow-md min-h-[170px]">
       <span>First test journal entry</span>
-      <div>
+      <div className="flex items-center justify-between">
         <small>Date of entry</small>
         <MdDeleteForever />
       </div>
--- src/components/journal/Journal.jsx
+++ src/components/journal/Journal.jsx
@@ -3,12 +3,14 @@ import Entry from "./Entry";
 
 const Journal = () => {
   return (
-    <div className="container">
-      <h1>Journal</h1>
-      <Entry />
-      <Entry />
-      <Entry />
-      <Entry />
+    <div className="w-full h-screen top-20 bg-cover bg-no-repeat bg-[url('../../public/img/background3.png')]">
+      <div className="max-w-[800px] w-full m-auto p-4">
+        <h1>Journal</h1>
+        <Entry />
+        <Entry />
+        <Entry />
+        <Entry />
+      </div>{" "}
     </div>
   );
 };
