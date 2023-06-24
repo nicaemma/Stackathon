491a7240e27d34909808015ac9439ad236568983
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-24 13:28:25 -0400

    Initial styling of journal entries

--- src/components/journal/Entry.jsx
+++ src/components/journal/Entry.jsx
@@ -3,7 +3,7 @@ import { MdDeleteForever } from "react-icons/md";
 
 const Entry = () => {
   return (
-    <div>
+    <div className="bg-[#f3e8ff] flex flex-col justify-between p-2 mb-10 border-r-10 min-h-[170px]">
       <span>First test journal entry</span>
       <div>
         <small>Date of entry</small>
