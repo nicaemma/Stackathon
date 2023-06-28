674b8e697822bb6cc3e344a09e1caa6391aadd22
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-28 16:56:56 -0400

    Additional styling of My Self Care

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -84,46 +84,48 @@ const Skills = () => {
 
   return (
     <div className="font-sora w-full h-screen top-20 bg-cover bg-no-repeat bg-[url('/img/background5.png')]">
-      <div className="max-w-[960px] m-auto p-3 flex flex-col gap-8 items-center">
-        <div className="p-3 max-w-[550px] flex flex-col items-center">
-          <h3 className="text-2xl font-bold text-center text-gray-800 p-3">
-            My Stress Relievers
-          </h3>
-          <div className="py-5 px-3 rounded-lg text-center bg-indigo-100 bg-opacity-90">
-            <div>
-              Coping skills help us navigate and overcome stressful moments with
-              increasing ease. Use this page to add stress relievers to your
-              personal list and track their use.
+      <div className="w-full h-screen top-20 bg-cover bg-white bg-opacity-50">
+        <div className="max-w-[960px] m-auto p-3 flex flex-col gap-8 items-center">
+          <div className="p-3 max-w-[550px] flex flex-col items-center">
+            <h3 className="text-2xl font-bold text-center text-gray-800 p-3">
+              My Stress Relievers
+            </h3>
+            <div className="py-5 px-3 rounded-lg text-center bg-indigo-200">
+              <div>
+                Coping skills help us navigate and overcome stressful moments
+                with increasing ease. Use this page to add stress relievers to
+                your personal list and track their use.
+              </div>
             </div>
           </div>
-        </div>
-        <form className="flex justify-between" onSubmit={createSkill}>
-          <input
-            value={newSkill}
-            className="border p-2 w-full text-xl rounded-md"
-            type="text"
-            placeholder="Add coping skill"
-            onChange={(e) => setNewSkill(e.target.value)}
-          />
-          <button
-            className="p-4 ml-2 rounded-full bg-indigo-300 hover:bg-indigo-400"
-            type="submit"
-          >
-            <AiOutlinePlus />
-          </button>
-        </form>
-      </div>
-      <div className="bg-slate-100 max-w-[400px] w-full m-auto rounded-md shadow-xl p-4">
-        <ul>
-          {skills.map((skill, index) => (
-            <Skill
-              key={index}
-              skill={skill}
-              toggleComplete={toggleComplete}
-              deleteSkill={deleteSkill}
+          <form className="flex justify-between" onSubmit={createSkill}>
+            <input
+              value={newSkill}
+              className="border-[1px] border-gray-400 p-2 w-full text-xl rounded-md"
+              type="text"
+              placeholder="Add coping skill"
+              onChange={(e) => setNewSkill(e.target.value)}
             />
-          ))}
-        </ul>
+            <button
+              className="p-4 ml-2 rounded-full bg-indigo-300 hover:bg-indigo-400"
+              type="submit"
+            >
+              <AiOutlinePlus />
+            </button>
+          </form>
+        </div>
+        <div className="bg-slate-100 max-w-[400px] w-full m-auto rounded-md shadow-xl p-4">
+          <ul>
+            {skills.map((skill, index) => (
+              <Skill
+                key={index}
+                skill={skill}
+                toggleComplete={toggleComplete}
+                deleteSkill={deleteSkill}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
