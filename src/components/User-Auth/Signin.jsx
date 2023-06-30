78302d9831004a1bcdaee08fa16b3e28cdea3993
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-29 21:07:09 -0400

    Font change in signup/signout

--- src/components/User-Auth/Signin.jsx
+++ src/components/User-Auth/Signin.jsx
@@ -25,7 +25,7 @@ const Signin = () => {
   }
 
   return (
-    <div className="'max-w-[700px] mx-auto my-16 p-4">
+    <div className="'max-w-[700px] font-fira mx-auto my-16 p-4">
       <div>
         <h1 className="text-2xl font-bold py-2">Sign into your account</h1>
         <p className="py-2">
